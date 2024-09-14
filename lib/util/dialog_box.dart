b706d91709ae8225e4b913674f1905118a291677
Derek <dberb789@gmail.com>
2024-09-14 22:33:03 +0800

    feat: add create task button and alertDialog

---
+++ lib/util/dialog_box.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class DialogBox extends StatelessWidget {
+  const DialogBox({super.key});
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Container(
+        height: 200,
+        width: 1000,
+        child: const Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Add task here",
+              ),
+            )
+          ],
+      )
+      ),
+    );
+  }
+}
